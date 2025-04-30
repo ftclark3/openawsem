Fixes burial term calculation for k_burial=0

openawsem/functionTerms/contactTerms.py
@@ -40,7 +40,7 @@ def inWhichChain(residueId, chain_ends):
             return chain_table[i]
 
 
-def contact_term(oa, k_contact=4.184, k_burial=4.184, z_dependent=False, z_m=1.5, inMembrane=False, membrane_center=0*angstrom, k_relative_mem=1.0, periodic=False, parametersLocation=None, burialPartOn=True, withExclusion=False, forceGroup=22,
+def contact_term(oa, k_contact=4.184, k_burial = None, z_dependent=False, z_m=1.5, inMembrane=False, membrane_center=0*angstrom, k_relative_mem=1.0, periodic=False, parametersLocation=None, burialPartOn=True, withExclusion=False, forceGroup=22,
                 gammaName="gamma.dat", burialGammaName="burial_gamma.dat", membraneGammaName="membrane_gamma.dat", r_min=0.45,min_sequence_separation=10,min_sequence_separation_mem=10):
     if parametersLocation is None:
         parametersLocation=openawsem.data_path.parameters
@@ -51,6 +51,16 @@ def contact_term(oa, k_contact=4.184, k_burial=4.184, z_dependent=False, z_m=1.5
         k_contact = k_contact * oa.k_awsem
     else:
         print(f"Unknown input, {k_contact}, {type(k_contact)}")
+    #setting burial
+    if k_burial == None:
+        k_burial = k_contact
+    elif isinstance(k_burial, float) or isinstance(k_burial, int):
+        k_burial = k_burial * oa.k_awsem   # just for backward comptable
+    elif isinstance(k_burial, Quantity):
+        k_burial = k_burial.value_in_unit(kilojoule_per_mole)   # convert to kilojoule_per_mole, openMM default uses kilojoule_per_mole as energy.
+        k_burial = k_burial * oa.k_awsem
+    else:
+        print(f"Unknown input, {k_burial}, {type(k_burial)}")
     # combine direct, burial, mediated.
     # default membrane thickness 1.5 nm
     membrane_center = membrane_center.value_in_unit(nanometer)   # convert to nm
@@ -1483,7 +1493,9 @@ def contact_term_shift_well_center(oa, k_contact=4.184, z_dependent=False, z_m=1
     contact.setForceGroup(forceGroup)
     return contact
 
-def burial_term(oa, k_burial=4.184, fastaFile="FastaFileMissing"):
+def burial_term(oa, k_burial=4.184, periodic=False, parametersLocation=None, burialGammaName="burial_gamma.dat", forceGroup=17):
+    if parametersLocation is None:
+        parametersLocation=openawsem.data_path.parameters
     k_burial *= oa.k_awsem
     burial_kappa = 4.0
     burial_ro_min = [0.0, 3.0, 6.0]
@@ -1492,7 +1504,7 @@ def burial_term(oa, k_burial=4.184, fastaFile="FastaFileMissing"):
     eta = 50  # eta actually has unit of nm^-1.
     r_min = .45
     r_max = .65
-    burial_gamma = np.loadtxt("burial_gamma.dat")
+    burial_gamma = np.loadtxt(os.path.join(parametersLocation, burialGammaName))
 
     # return burial
     # if ( lc->chain_no[i]!=lc->chain_no[j] || abs(lc->res_no[j] - lc->res_no[i])>1 )
@@ -1539,7 +1551,14 @@ def burial_term(oa, k_burial=4.184, fastaFile="FastaFileMissing"):
         for e2 in cb_fixed:
             burial.addExclusion(e1, e2)
 
-    burial.setForceGroup(17)
+    if periodic:
+        burial.setNonbondedMethod(burial.CutoffPeriodic)
+        print('\ncontact_term is periodic')
+    else:
+        burial.setNonbondedMethod(burial.CutoffNonPeriodic)
+    print("Burial cutoff ", burial.getCutoffDistance())
+    print("NonbondedMethod: ", burial.getNonbondedMethod())
+    burial.setForceGroup(forceGroup)
     return burial
 
 '''