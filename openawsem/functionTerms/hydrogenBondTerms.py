61e309e8b04585dfc5ad7a11fab998bc222cd681
Finley Clark <finn2001clark@gmail.com>
2025-08-27 22:47:53 -0500

    missed one parameter in the previous commit

--- openawsem/functionTerms/hydrogenBondTerms.py
+++ openawsem/functionTerms/hydrogenBondTerms.py
@@ -817,7 +817,7 @@ def _beta_lammps_awsemmd(oa, term_number, ssweight_file, forceGroup, k_beta, bet
     return Beta
 
 def _inner_loop_eoc(i,nres,chain_starts,chain_ends,rama_biases,
-                      p_par, p_anti, p_antihb, p_antinhb, p_parhb,):
+                      p_par, p_anti, p_antihb, p_antinhb, p_parhb, a):
     """
     Helper function for _beta_efficiency_optimized
     """
